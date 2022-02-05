ea2aaed30f022b6f9a18d433dfc5ff86cb85cbc6
e1900303 <tranduyphat080300@gmail.com>
2022-02-05 04:11:32 +0200

    commit

--- index.js
+++ index.js
@@ -32,8 +32,9 @@ app.use(express.json())
 morgan.token('body', (req, res)=> JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('build'))
 
-  
+
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id===id)
