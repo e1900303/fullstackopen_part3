304ba891a728f61cf4b4566895efe3a466c24ae3
e1900303 <tranduyphat080300@gmail.com>
2022-02-06 02:40:05 +0200

    added mongo.js

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://tduyphat:${password}@fullstackopen.bhss4.mongodb.net/persons?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Persons', personSchema)
+
+if(process.argv.length === 5) {
+    
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const person = new Person({ name, number })
+    
+    person.save().then(person => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      mongoose.connection.close()
+    })
+  }
+
+if(process.argv.length === 3) {
+    console.log('phonebook:')
+  
+    Person
+      .find({})
+      .then(persons => {
+        persons.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        });
+        mongoose.connection.close()
+      });
+  }
